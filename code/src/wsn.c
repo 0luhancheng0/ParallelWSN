7a04ec3fff0d218be1e271a5c2009f3dfa7b45eb
Luhan Cheng <lche0021@student.monash.edu>
2019-09-19 19:08:29 +1000

    d

--- code/src/wsn.c
+++ code/src/wsn.c
@@ -3,21 +3,7 @@
 #include <time.h>
 #include <mpi.h>
 #include <unistd.h>
-#define X_SIZE 2
-#define Y_SIZE 3
-
-// the rank of base station in MPI_COMM_WORLD
-#define BASERANK 0
-
-// the number of bits each random number have, which also defines the upper bound of random number as 2^n-1, maximum 1byte
-#define N_BIT_RAND 4
-
-// run N_INTERATION number times
-#define N_ITERATION 10000
-
-// run sleep ITERVAL millisecond each iteration
-#define INTERVAL 100
-
+#include "config.h"
 struct logging
 {
     int n_event;
