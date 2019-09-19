59515bbff1f053e60ebb84310ee896a63bba648b
Luhan Cheng <lche0021@student.monash.edu>
2019-09-19 23:23:39 +1000

    d

---
+++ code/src/build/tiny-AES-c/CMakeFiles/tiny-aes.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/ChengLuhan/Desktop/Units/FIT3143/Assignment/Assignment_2/fit3143_assignment2/code/src/tiny-AES-c/aes.c" "/Users/ChengLuhan/Desktop/Units/FIT3143/Assignment/Assignment_2/fit3143_assignment2/code/src/build/tiny-AES-c/CMakeFiles/tiny-aes.dir/aes.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../tiny-AES-c/tiny-AES-c"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/src/build/tiny-AES-c/CMakeFiles/tiny-aes.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tiny-aes.dir/aes.c.o"
+  "libtiny-aes.pdb"
+  "libtiny-aes.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tiny-aes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
