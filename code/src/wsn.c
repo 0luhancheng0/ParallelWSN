5b3e69569285e51894471d22b9a944855411844e
Luhan Cheng <lche0021@student.monash.edu>
2019-09-26 10:12:32 +1000

    d

--- code/src/wsn.c
+++ code/src/wsn.c
@@ -5,19 +5,6 @@
 #include <mpi.h>
 #include <unistd.h>
 #include "wsn.h"
-typedef struct logging
-{
-    int n_event;
-    int* enent_num;
-    int* reference_rank;
-} logging;
-
-struct event_message 
-{
-    int event_num;
-    int my_rank;
-    int* reference_rank;
-};
 void get_neighbor_count(int coord[2], int* neighbor_count) {
 	int *neibhbor_count;
 	// MPI_Cart_shift()	
@@ -41,7 +28,7 @@ void get_neighbor_count(int coord[2], int* neighbor_count) {
 
 int main(int argc, char *argv[])
 {
-    int global_size, global_rank, size, rank, nneighbor, colored_rank, colored_size;
+    int global_size, global_rank, size, rank, nneighbor;
     uint8_t random_num;
     uint8_t* neighbor_result;
     MPI_Comm node_comm;
@@ -49,12 +36,15 @@ int main(int argc, char *argv[])
     const int baserank_list[1] = {BASERANK};
     int dim[2] = {X_SIZE, Y_SIZE}, period[2] = {0}, coord[2], reorder = 0;
     int* neighbor_rank;
+	MPI_Datatype message_type;
+	
     MPI_Group global_group, node_group;
     MPI_Request *reqs;
-    // MPI_Request reqs[4];
     MPI_Request trash_req;
 
     MPI_Init(&argc, &argv);
+	MPI_Type_contiguous(MESSAGE_LEN, MPI_UINT8_T, &message_type);
+	MPI_Type_commit(&message_type);
     MPI_Comm_size(MPI_COMM_WORLD, &global_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &global_rank);
     
@@ -76,13 +66,14 @@ int main(int argc, char *argv[])
         srand(time(NULL)+rank);
 
         random_num = (uint8_t)(rand() & ((1 << N_BIT_RAND) - 1));
-
         get_neighbor_count(coord, &nneighbor);
 
         printf("Random number is %d on rank %d coordinates are %d %d. Has %d neighbor\n", random_num, rank, coord[0], coord[1], nneighbor);
         MPI_Alloc_mem((MPI_Aint)(nneighbor * sizeof(MPI_UINT8_T)), MPI_INFO_NULL, &neighbor_result);
         MPI_Alloc_mem((MPI_Aint)(sizeof(MPI_Request) * 4), MPI_INFO_NULL, &reqs);
         MPI_Alloc_mem(nneighbor, MPI_INFO_NULL, &neighbor_rank);
+		
+		// similar implementation with MPI_Ineighbor_alltoall
         for (int i=0, dim=0; dim<2; ++dim) {
             MPI_Cart_shift(node_comm, dim, 1, &r0, &r1);
             if (r0 >= 0) {
@@ -102,13 +93,11 @@ int main(int argc, char *argv[])
         for (int i=0;i <nneighbor; i++) {
             printf("rank %d received %u from %d\n", rank, neighbor_result[i], neighbor_rank[i]);
         }
-		// MPI_Free_mem(&neighbor_result);
-		// MPI_Free_mem(&neighbor_rank);
-		// MPI_Free_mem(&reqs);
 		MPI_Comm_free(&node_comm);
-        // MPI_Finalize();
-        // return(0);
-    }
+    } else {
+		printf("Rank of base station is %d\n", global_rank);
+	
+	}
     
     MPI_Finalize();
     return(0);
