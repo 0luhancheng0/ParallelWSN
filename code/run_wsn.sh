6889aa72d8d9a52a82ccd0e6b737ab7940f7c5a4
Luhan Cheng <0lh.cheng0@gmail.com>
2019-10-17 00:02:58 +1100

    d

--- code/run_wsn.sh
+++ code/run_wsn.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --ntasks=21
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=2GB
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=lche0021@student.monash.edu
-
+#SBATCH --time=00:20:00
 hostname
+module load mpip
 make build
-make run OMP_NUM_THREADS=4
+make run OMP_NUM_THREADS=2
