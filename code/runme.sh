27e02db10b2d0cfc1e9be358a92b27471718f90b
Luhan Cheng <0lh.cheng0@gmail.com>
2019-10-19 21:18:20 +1100

    d

---
+++ code/runme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --ntasks=21
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=lche0021@student.monash.edu
+#SBATCH --time=00:20:00
+OMP_NUM_THREADS=1
+mpirun -n 21 -x OMP_NUM_THREADS=$(OMP_NUM_THREADS) ./wsn
